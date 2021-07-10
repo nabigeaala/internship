2056c2d40717708d48fc7af0569bd1bcf6b22255
nabigeaala <51216848+nabigeaala@users.noreply.github.com>
2021-07-10 16:14:38 +0530

    Create CheckPalindrome.py

---
+++ 10th-july/CheckPalindrome.py
@@ -0,0 +1,18 @@
+#@purposeOfCode to check if a string is palindrome or not
+#@techCategory python
+#@code
+my_str = 'aIbohPhoBiA'
+
+# make it suitable for caseless comparison
+my_str = my_str.casefold()
+
+# reverse the string
+rev_str = reversed(my_str)
+
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")
+    
+#@output The string is a palindrome.    
